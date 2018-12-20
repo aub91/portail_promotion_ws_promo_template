35acab105cb61a514b0f4926cffcd47ac4810480
damienfo <damien!.foucher93@gmail.com>
2018-12-20 15:20:27 +0100

    construction premiers packages

---
+++ src/main/java/fr/afcepf/al32/wspromotemplate/app/App.java
@@ -0,0 +1,18 @@
+package fr.afcepf.al32.wspromotemplate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+@SpringBootApplication(scanBasePackages= {"fr.afcepf.al32.groupe2"})
+public class App extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		
+	}
+
+}
