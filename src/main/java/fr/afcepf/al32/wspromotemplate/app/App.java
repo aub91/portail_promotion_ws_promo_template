e15b7408dedbefc29143f3b1474b4f6651f825ff
aub91 <aubin.guilhem@gmail.com>
2018-12-21 14:46:34 +0100

    Modify entities to avoid infinite loop and use of List instead of Map

--- src/main/java/fr/afcepf/al32/wspromotemplate/app/App.java
+++ src/main/java/fr/afcepf/al32/wspromotemplate/app/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 
-@SpringBootApplication(scanBasePackages= {"fr.afcepf.al32.groupe2"})
+@SpringBootApplication(scanBasePackages= {"fr.afcepf.al32.wspromotemplate"})
 public class App extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
